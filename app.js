a6793f439db67091f6270b107393035a4a3fb7c1
brugobi <brgobbi@hotmail.com>
2020-10-23 16:16:15 -0400

    add simple logic

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn"); //id
+const color = document.querySelector('.color'); //class
+
+btn.addEventListener('click', function () {
+  // seletc random number between 0 - 3
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
